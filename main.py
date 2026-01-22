fe188ae0d495ecf9f04ba8612822b552e7ecd568
lucco <luccondevaux@gmail.com>
2026-01-22 01:44:47 -0500

    work done on 1/21 and night after

--- main.py
+++ main.py
@@ -1,78 +1,90 @@
-from turtle import *
+import turtle
+import time
 
 #start stuff here:
-
 Window = turtle.Screen()
 
 #initializing information here:
 
 class Particle:
-    def __init__(self, mass, x_coord, y_coord, z_coord, x_vel, y_vel, z_vel, turtle):
-        self.turtle = turtle.Turtle()
-        #mass in quectograms, 10^-30 grams
-        coord = [self.x_coord, self.y_coord, self.z_coord]
-        vel = [self.x_vel, self.y_vel, self.z_vel]
+    def __init__(self, coord, vel):
+        pass#mass in quectograms, 10^-30 grams
     
-    def change_vel_charge(charged_particle_list):
-        for particle in charged_particle_list:
-            for vect_num in range(len(coord)):
-                vect_disp = coord[vect_num] - particle.coord[vect_num]
-                if vect_disp != 0:
-                    vel_change += (charge * particle.charge) / (vect_disp**2)
-    
-    def change_pos():
-        for i in range(len(coord)):
-            coord[i] += vel[i]
+    def change_pos(self):
+        for i in range(len(self.coord)):
+            self.coord[i] += self.vel[i]
 
 
 class Charged_Particle(Particle):
-    def __init__(self, mass, x_coord, y_coord, z_coord, x_vel, y_vel, z_vel, charge, turtle):
-        super().__init__(mass, x_coord, y_coord, z_coord, x_vel, y_vel, z_vel, charge, turtle)
+    def __init__(self, coord, vel):
+        super().__init__(coord, vel)
+    
+    def change_vel_charge(self, particle_list):
+        for particle in particle_list:
+            for vect_num in range(len(self.coord)):
+                vect_disp = self.coord[vect_num] - particle.coord[vect_num]
+                if vect_disp != 0:
+                    self.vel[vect_num] += (self.charge * particle.charge) / (vect_disp**2)
+
 
 class Proton(Charged_Particle):
-    def __init__(self, mass, x_coord, y_coord, z_coord, x_vel, y_vel, z_vel, turtle, charge):
-        super().__init__(mass, x_coord, y_coord, z_coord, x_vel, y_vel, z_vel, turtle, charge)
+    def __init__(self, coord, vel):
+        super().__init__(coord, vel)
         self.charge = 1
         self.mass = 1,672,621.72
+        self.coord = coord
+        self.vel = vel
 
 class Electron(Charged_Particle):
-    def __init__(self, mass, x_coord, y_coord, z_coord, x_vel, y_vel, z_vel, turtle, charge):
-        super().__init__(mass, x_coord, y_coord, z_coord, x_vel, y_vel, z_vel, turtle, charge)
+    def __init__(self, coord, vel):
+        super().__init__(coord, vel)
         self.charge = -1
         self.mass = 910.9389699
+        self.coord = coord
+        self.vel = vel
 
 class Neutron(Charged_Particle):
-    def __init__(self, mass, x_coord, y_coord, z_coord, x_vel, y_vel, z_vel, turtle, charge):
-        super().__init__(mass, x_coord, y_coord, z_coord, x_vel, y_vel, z_vel, turtle, charge)
+    def __init__(self, coord, vel):
+        super().__init__(coord, vel)
         self.charge = 0
         self.mass = 1674927.5
+        self.coord = coord
+        self.vel = vel
 
 #main run here:
 
-Window.exitonclick()
+#Window.exitonclick()
+print("1got here")
 
-particle_list = []
-charged_particle_list = []
+particle_list = [Proton(coord=[0, 0, 0], vel=[0, 0, 0]), Proton(coord=[100, 100, 0], vel=[0, 0, 0])]
+print("2got here")
 
 particle_turtles = []
-charged_particle_turtles = []
+print("3got here")
 
 clicked = False
-for particle in particle_list:
-    particle_turtles.append(Turtle())
+for i in range(len(particle_list)):
+    particle_turtles.append(turtle.Turtle())
+    #particle_turtles[i].up()
+    particle_turtles[i].shape("circle")
+    print("turtle added")
+print("4got here")
 
-for particle in particle_list:
-    if particle.charge != 0:
-        charged_particle_list.append(particle)
-        charged_particle_turtles.append(Turtle())
+seconds = 0
 
-while not clicked:
+while seconds > 10:
     #code here
+    print("5got here")
+    time.sleep(1)
+    seconds +=1
     for i in range(len(particle_list)):
-        particle_list[i].change_vel_charge(charged_particle_list)
+        print("6got here")
+        particle_list[i].change_vel_charge(particle_list)
         #change_vel_nucl here
         particle_list[i].change_pos()
         #figure out how to compute x, y pos, how to change size for z pos
+        particle_turtles[i].goto(particle_list[i].coord[0], particle_list[i].coord[1])
+        turtle.update()
 
-
-    Window.onscreenclick(clicked != clicked)
+print("tf")
+turtle.mainloop()
